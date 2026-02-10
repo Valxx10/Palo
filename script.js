abc34c503f66121c63b3b45ffb4b92aaa3c4b5f9
Valxx10 <restrepovalentina414@gmail.com>
2026-02-09 21:26:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function init() {
 			container.addChild(heart);
 		}
 
-  var text = new createjs.Text("the longer I'm with you\nthe more I love you", "bold 24px Arial", "#fff");
+  var text = new createjs.Text("will you be my valentine Palo, I love you", "bold 24px Arial", "#fff");
   text.textAlign = "center";
   text.x = w / 2;
   text.y = h / 2 - text.getMeasuredLineHeight();
